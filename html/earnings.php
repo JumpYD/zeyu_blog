695085b50ed7d604dd7841837b6463646eb864bc
zeyu203 <zeyu203@qq.com>
2014-10-05 19:44:00 +0800

    财报页面每页最多显示12个月财报，并且增加查询与绘图

--- html/earnings.php
+++ html/earnings.php
@@ -4,30 +4,53 @@ require_once (dirname(__FILE__).'/'.'head.php');
 global $smarty;
 LogOpt::init('display_earnings', true);
 
-$query = 'select * from earnings order by month desc';
+$det_beg = empty($_GET['beg']) ? date('Y-m', time()) : $_GET['beg'];
+$det_end = empty($_GET['end']) ? '2013-09' : $_GET['end'];
+if ($det_beg > $det_end)
+	list($det_beg, $det_end) = array($det_end, $det_beg);
+$drw_beg = empty($_GET['drw_beg']) ? date('Y-m', time()) : $_GET['drw_beg'];
+$drw_end = empty($_GET['drw_end']) ? '2013-09' : $_GET['drw_end'];
+if ($drw_beg > $drw_end)
+	list($drw_beg, $drw_end) = array($drw_end, $drw_beg);
+
+$query = 'select * from earnings order by month desc limit 24';
 $earnings = MySqlOpt::select_query($query);
 $earn_infos = array();
 $month = array();
 $income = array();
 $expend = array();
+
+$index = 0;
+
 foreach ($earnings as $earning)
 {
 	$infos = array();
-	$infos['idx_href'] = 'article.php?id='.$earning['article_id'];
-	$image_select_query = 'select path from images where image_id='.$earning['image_id'];
-	$image_ret = MySqlOpt::select_query($image_select_query);
-	$path = $image_ret[0]['path'];
-	$infos['image_path'] = $path;
-	$infos['title'] = $earning['month'];
-	$infos['descs'] = '结余:&nbsp;&nbsp;'.($earning['income']-$earning['expend']);
-	$earn_infos[] = $infos;
+	if ($earning['month'] >= $det_beg and $earning['month'] <= $det_end and $index < 12)
+	{
+		$infos['idx_href'] = 'article.php?id='.$earning['article_id'];
+		$image_select_query = 'select path from images where image_id='.$earning['image_id'];
+		$image_ret = MySqlOpt::select_query($image_select_query);
+		$path = $image_ret[0]['path'];
+		$infos['image_path'] = $path;
+		$infos['title'] = $earning['month'];
+		$infos['descs'] = '结余:&nbsp;&nbsp;'.($earning['income']-$earning['expend']);
+		$earn_infos[] = $infos;
+		$index++;
+	}
 
-	$month[] = $earning['month'];
-	$income[] = $earning['income'];
-	$expend[] = $earning['expend'];
+	if ($earning['month'] >= $drw_beg and $earning['month'] <= $drw_end)
+	{
+		$month[] = $earning['month'];
+		$income[] = $earning['income'];
+		$expend[] = $earning['expend'];
+	}
 }
 $average = round((array_sum($income)-array_sum($expend))/count($month), 2);
 
+$smarty->assign('det_beg_month', $det_beg);
+$smarty->assign('det_end_month', $det_end);
+$smarty->assign('drw_beg_month', $drw_beg);
+$smarty->assign('drw_end_month', $drw_end);
 $smarty->assign('labels', json_encode(array_reverse($month)));
 $smarty->assign('income', json_encode(array_reverse($income)));
 $smarty->assign('expend', json_encode(array_reverse($expend)));
--- views/note.tpl
+++ views/note.tpl
@@ -1,5 +1,28 @@
 <{include 'header.tpl'}>
+<script src="../resource/bootstrap/js/jquery.js"></script>
+<script language="javascript">
+function get_earnings()
+{
+	var det_beg_month = $('#beg_month').val();
+	var det_end_month = $('#end_month').val();
+	var drw_beg_month = $('#drw_beg_month').val();
+	var drw_end_month = $('#drw_end_month').val();
+	window.location.href = 'earnings.php?beg='+det_beg_month+'&'+'end='+det_end_month+'&drw_beg='+drw_beg_month+'&drw_end='+drw_end_month;
+}
+function drw_earnings()
+{
+	var det_beg_month = $('#det_beg_month').val();
+	var det_end_month = $('#det_end_month').val();
+	var drw_beg_month = $('#beg_month').val();
+	var drw_end_month = $('#end_month').val();
+	window.location.href = 'earnings.php?beg='+det_beg_month+'&'+'end='+det_end_month+'&drw_beg='+drw_beg_month+'&drw_end='+drw_end_month;
+}
+</script>
 <br /><br /><br />
+<input type="hidden" id="det_beg_month" class="form-control" value="<{$det_beg_month}>"/>
+<input type="hidden" id="det_end_month" class="form-control" value="<{$det_end_month}>"/>
+<input type="hidden" id="drw_beg_month" class="form-control" value="<{$drw_beg_month}>"/>
+<input type="hidden" id="drw_end_month" class="form-control" value="<{$drw_end_month}>"/>
 <div class="container projects">
 	<div class="row">
 	<{*
@@ -12,8 +35,24 @@
 		<{if $category_id==1}>
 			<canvas id="myChart" width="980px" height="253px" class="span12"></canvas>
 			<div class='span12'><br /></div>
-			<div class='span8'></div><div class='span2'><input type="text" class="form-control" placeholder="Text input" readonly='readonly' value='平均月结余：<{$average}>'\></div>
-			<div class='span12'><br /><br /></div>
+			<div class='span2'>
+				<input type="text" id="beg_month" class="form-control" placeholder="BEG_MONTH" value="<{$beg_month}>"/>
+			</div>
+			<div class='span2'>
+				<input type="text" id="end_month" class="form-control" placeholder="END_MONTH" value="<{$end_month}>"/>
+			</div>
+			<div class='span2'>
+				<button type="submit" onclick="get_earnings()" class="btn btn-default">详&nbsp;&nbsp;情</button>&nbsp;&nbsp;
+				<button type="submit" onclick="drw_earnings()" class="btn btn-default">绘&nbsp;&nbsp;图</button>
+			</div>
+			<div class='span2'>
+			</div>
+			<div class='span2'>
+				<input type="text" class="form-control" placeholder="Text input" readonly='readonly' value='平均月结余：<{$average}>'\>
+			</div>
+			<div class='span12'>
+				<br /><br />
+			</div>
 		<{/if}>
 		<{foreach item=info from=$infos}>
 		<div class="col-sm-6 col-md-4 ">
